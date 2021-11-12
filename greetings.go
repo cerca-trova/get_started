17f4ece88a265415a293128f9f8856ec516b9984
chenran <1044878337cr@sina.com>
2021-11-12 13:31:30 +0800

    if name not given, raise an error

--- greetings.go
+++ greetings.go
@@ -1,11 +1,16 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Function Hello returns a greetings for the names person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If name was not given, raise an error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	message := fmt.Sprintf("Hi,%v. Welcome!", name)
-	return message
+	return message, nil
 }
